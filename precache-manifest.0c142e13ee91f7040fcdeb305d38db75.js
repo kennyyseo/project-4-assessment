4dd1527c94a00ed217ff30370e680198575b38f4
Kenny Seo <kennyyseo@gmail.com>
2020-10-16 13:26:57 -0700

    Updates

---
+++ precache-manifest.0c142e13ee91f7040fcdeb305d38db75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d4b09c46bb6e8f02aabc926d6d1a1ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5835b738821335d1bb73",
+    "url": "/static/css/main.75adaa61.chunk.css"
+  },
+  {
+    "revision": "608118624ae7ee197d4b",
+    "url": "/static/js/2.e208ecfd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.e208ecfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5835b738821335d1bb73",
+    "url": "/static/js/main.bf85b2a3.chunk.js"
+  },
+  {
+    "revision": "e968eeb8c247134aad12",
+    "url": "/static/js/runtime-main.bdad0aff.js"
+  }
+]);
